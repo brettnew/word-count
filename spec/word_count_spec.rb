85fc1147fd5397b2d0c8632c6038304f9c51ff54
Brett New <brett.new91@gmail.com>
2016-07-01 08:57:02 -0700

    Add and pass fourth spec.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -11,4 +11,7 @@ describe('String#word_count') do
   it('will compare to string with multiple same words and return how many times first string appears in the second string') do
     expect('cat'.word_count('good cat bad cat mad cat sad cat')).to(eq(4))
   end
+  it('will account for numbers and spaces') do
+    expect('cat1'.word_count('good cat9 bad cat1 mad c a t sad cat')).to(eq(1))
+  end
 end
