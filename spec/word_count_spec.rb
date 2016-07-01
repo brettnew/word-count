de2eb66166219fb2248f1cf18bca5cc8a66f2ab0
Brett New <brett.new91@gmail.com>
2016-07-01 08:46:35 -0700

    Refactor code to clean it up

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -9,6 +9,6 @@ describe('String#word_count') do
     expect('cat'.word_count('cat attack to come')).to(eq(1))
   end
   it('will compare to string with multiple same words and return how many times first string appears in the second string') do
-    expect('cat'.word_count('cat cat cat cat')).to(eq(4))
+    expect('cat'.word_count('good cat bad cat mad cat sad cat')).to(eq(4))
   end
 end
