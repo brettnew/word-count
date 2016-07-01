6436956843f4e35d0df0b68abb434753234fac05
Brett New <brett.new91@gmail.com>
2016-07-01 09:25:39 -0700

    add layout and results page; integration spec passes.

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -6,9 +6,9 @@ set(:show_exceptions, false)
 describe('the word count path', {:type => :feature}) do
   it('processes user entry and returns how many times the first input appears in the second input') do
     visit('/')
-    fill_in('initial_string', :with => 'peck')
+    fill_in('initial_word', :with => 'peck')
     fill_in('word_list', :with => 'If Peter Piper picked a peck of pickled peppers, wheres the peck of pickled peppers Peter Piper picked?')
     click_button('Find')
-    expect(page).to have_content('Your initial word "peck" appeared 2 times')
+    expect(page).to have_content("Your initial word \"peck\" apppeared 2 times")
   end
 end
