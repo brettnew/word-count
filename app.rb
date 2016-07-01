530059fd3ffe22f129c0e6fc1ae1a73745a84b66
Brett New <brett.new91@gmail.com>
2016-07-01 09:15:11 -0700

    Create integration specs and app.rb

--- app.rb
+++ app.rb
@@ -2,3 +2,14 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/word_count')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/word_count') do
+  @initial_word = params.fetch('initial_word')
+  @word_list = params.fetch('word_list')
+  @result = @initial_word.word_count(@word_list)
+  erb(:word_count)
+end
