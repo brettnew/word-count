9689e9a156ded39e49311ebb30630f5d18cb513a
Brett New <brett.new91@gmail.com>
2016-07-01 08:42:29 -0700

    Write and pass second spec to deal with multiple words in a string.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -2,15 +2,19 @@ require('rspec')
 
 class String
   define_method(:word_count) do |words_list|
-    initial_string = self.split(" ")
+    initial_string = self
     words_list = words_list.split(" ")
     counter = 0
     new_array = []
     words_list.each() do |word|
-      if initial_string == words_list
+      # word_array = word.split(" ")
+      if initial_string == word
         counter = counter.+(1)
+      else
+        new_array.push(word)
       end
     end
     counter
+    # new_array
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('word_count')
 
 describe('String#word_count') do
-  it('will detect the matching word in a string and return how many times that string appears') do
+  it('will take the user input string, compare to identical second user input string and return how many times first string appears in the second string') do
     expect('cat'.word_count('cat')).to(eq(1))
+  end
+  it('will take the user input string, compare to second user input string with multiple words and return how many times first string appears in the second string') do
+    expect('cat'.word_count('cat attack to come')).to(eq(1))
   end
 end
