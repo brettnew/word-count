4e565a65202c86a7af70483335c74ffc3ff1a6fb
Brett New <brett.new91@gmail.com>
2016-07-01 08:34:41 -0700

    Add and pass first spec

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,1 +1,16 @@
 require('rspec')
+
+class String
+  define_method(:word_count) do |words_list|
+    initial_string = self.split(" ")
+    words_list = words_list.split(" ")
+    counter = 0
+    new_array = []
+    words_list.each() do |word|
+      if initial_string == words_list
+        counter = counter.+(1)
+      end
+    end
+    counter
+  end
+end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('word_count')
 
 describe('String#word_count') do
-  it('will detect the matching word in a string') do
-    expect(cat)
+  it('will detect the matching word in a string and return how many times that string appears') do
+    expect('cat'.word_count('cat')).to(eq(1))
   end
 end
